b90523ea1276e4e32c664795d1c95970b2ce82a2
Amit Sisodiya <amitsisodiya275@gmail.com>
2021-10-05 21:54:54 +0530

    Added: a Welcome file which can print welcome message on the console.

---
+++ main/java/com/bridgelab/linkedlist/Welcome.java
@@ -0,0 +1,10 @@
+package com.bridgelab.linkedlist;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to day 14 problems in this I will create LinkedList.");
+	}
+
+}
